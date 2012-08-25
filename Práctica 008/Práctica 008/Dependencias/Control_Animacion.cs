0449a8c1693d7c24c758fa4f7fe558c109253ff1
Alfonso Andrés <osnoser_12@hotmail.com>
2012-08-25 17:01:44 -0430

    Comienzo del proyecto
    Comienzo del proyecto para aprender y mejorar la forma de programar.
    Mini Megaman en C#.

---
+++ Práctica 008/Práctica 008/Dependencias/Control_Animacion.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+// ReSharper disable CheckNamespace
+namespace Dependencias
+// ReSharper restore CheckNamespace
+{
+    public class ControlAnimacion
+    {
+
+        private int Paso { get; set; }
+        public List<int> Cuadros { get; private set; }
+        public int FrameTime
+        {
+            get { return _frameTime; }
+        }
+        // The time we display a frame until the next one
+        private int _frameTime;
+        // The time since we last updated the frame
+        private int _elapsedTime;
+        public int CuadroActual
+        {
+            get
+            {
+                return Cuadros[Paso];
+            }
+        }
+        public bool PrimerCuadro
+        {
+            get { return Paso == 0; }
+        }
+
+        public ControlAnimacion(string frames, int frameTime)
+        {
+            this._frameTime = frameTime;
+            Cuadros = new List<int>();
+            var framesTmp = frames.Split(',');
+            foreach (var frame in framesTmp)
+                Cuadros.Add(int.Parse(frame));
+            Cuadros.Add(-1);
+        }
+
+        public bool Update(ref GameTime gameTime)
+        {
+            // Update the elapsed time
+            _elapsedTime += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+            // If the elapsed time is larger than the frame time
+            // we need to switch frames
+            if (_elapsedTime > _frameTime)
+            {
+                if (Cuadros[++Paso] == -1)
+                {
+                    Paso = 0;
+                    // If we are not looping deactivate the animation
+                    return true;
+                }
+                // Reset the elapsed time to zero
+                _elapsedTime = 0;
+            }
+            return false;
+        }
+
+        public void Reiniciar()
+        {
+            _elapsedTime = 0;
+            Paso = 0;
+        }
+
+    }
+}
---
+++ Práctica 008/Práctica 008/Dependencias/Personaje.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dependencias;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Práctica_008;
+
+// ReSharper disable CheckNamespace
+namespace Dependencias
+// ReSharper restore CheckNamespace
+{
+
+    public class Personaje
+    {
+        public enum Participantes
+        {
+            Megaman,
+            BeeBlader
+        }
+        public enum Estados
+        {
+            Parado,
+            Caminar,
+            Saltar,
+            Morir,
+            Eliminar,
+            Golpear
+        }
+        protected Estados EstadoAnterior;
+        protected Vector2 Posicion;
+        public Estados EstadoActual { get; protected set; }
+        protected Imagen Imagen { get; set; }
+        public float VelocidadSalto { get; protected set; }
+        protected Dictionary<Estados, ControlAnimacion> Animaciones;
+        protected bool Izquierda
+        {
+            get { return !Derecha; }
+            set { Derecha = !value; }
+        }
+        protected bool Derecha { get; set; }
+
+        public Personaje(Vector2 posicion, bool derecha)
+        {
+            Posicion = posicion;
+            Derecha = derecha;
+        }
+
+        public virtual void Update(Game1 juego) { }
+
+        public void Draw(SpriteBatch g)
+        {
+            Imagen.Update((int)EstadoActual, Animaciones[EstadoActual].CuadroActual);
+            Imagen.Draw(g, Derecha);
+        }
+
+    }
+}
