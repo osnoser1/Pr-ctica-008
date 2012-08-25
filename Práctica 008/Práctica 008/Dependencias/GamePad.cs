007ab08cefa92d899acacec85b1802220af259c0
Alfonso Andrés <osnoser_12@hotmail.com>
2012-08-25 17:05:26 -0430

    Revert "Revert "Comienzo del proyecto""
    This reverts commit 4b3565dbdc9e531a695fc9bc1193591733720bcd.

---
+++ Práctica 008/Práctica 008/Dependencias/GamePad.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+// ReSharper disable CheckNamespace
+namespace Dependencias
+// ReSharper restore CheckNamespace
+{
+    public class GamePad
+    {
+
+    }
+}
